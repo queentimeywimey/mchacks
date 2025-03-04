b9e7f8f747edb627f6968dd3df8766960ad01a56
Denali101 <denalibhms@gmail.com>
2025-03-04 16:44:04 -0500

    Change origin

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,7 +9,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://mchacks-rho.vercel.app/",
         methods: ["GET", "POST"]
     }
 });
